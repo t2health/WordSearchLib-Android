f02fe99d2313eef4a0cad4c9bda74a4998a14eec
wturney <westloos@comcast.net>
2012-07-31 11:09:43 -0700

    Warning fix/removal pass

--- src/com/t2/wordsearch/WordsearchLayoutAnimationController.java
+++ src/com/t2/wordsearch/WordsearchLayoutAnimationController.java
@@ -1,3 +1,4 @@
+
 package com.t2.wordsearch;
 
 import android.content.Context;
@@ -7,46 +8,43 @@ import android.view.animation.LayoutAnimationController;
 
 public class WordsearchLayoutAnimationController extends LayoutAnimationController {
 
-	private static final int[] INDEXES = new int[] {
-			2, 2, 3, 3, 3, 3, 2, 2,
-			2, 2, 2, 2, 2, 2, 2, 2,
-			0, 1, 1, 1, 1, 1, 1, 0,
-			0, 0, 1, 0, 0, 1, 0, 0,
-			0, 0, 1, 0, 0, 1, 0, 0,
-			0, 1, 1, 1, 1, 1, 1, 0,
-			2, 2, 2, 2, 2, 2, 2, 2,
-			2, 2, 3, 3, 3, 3, 2, 2
-	};
+    private static final int[] INDEXES = new int[] {
+            2, 2, 3, 3, 3, 3, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2,
+            0, 1, 1, 1, 1, 1, 1, 0,
+            0, 0, 1, 0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0, 1, 0, 0,
+            0, 1, 1, 1, 1, 1, 1, 0,
+            2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 3, 3, 3, 3, 2, 2
+    };
 
-	/**
-	 * @param animation
-	 * @param delay
-	 */
-	private WordsearchLayoutAnimationController(Animation animation, float delay) {
-		super(animation, delay);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param animation
+     * @param delay
+     */
+    private WordsearchLayoutAnimationController(Animation animation, float delay) {
+        super(animation, delay);
+    }
 
-	/**
-	 * @param animation
-	 */
-	public WordsearchLayoutAnimationController(Animation animation) {
-		super(animation);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param animation
+     */
+    public WordsearchLayoutAnimationController(Animation animation) {
+        super(animation);
+    }
 
-	/**
-	 * @param context
-	 * @param attrs
-	 */
-	public WordsearchLayoutAnimationController(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param context
+     * @param attrs
+     */
+    public WordsearchLayoutAnimationController(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	@Override
-	protected int getTransformedIndex(android.view.animation.LayoutAnimationController.AnimationParameters params) {
-		return INDEXES[params.index];
-	}
+    @Override
+    protected int getTransformedIndex(android.view.animation.LayoutAnimationController.AnimationParameters params) {
+        return INDEXES[params.index];
+    }
 
 }
